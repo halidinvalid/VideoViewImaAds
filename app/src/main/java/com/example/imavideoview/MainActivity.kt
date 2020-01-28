cfe250c436e4b2f107e9b107b289727809d47af5
Arda <arda.talay@partner.turktelekom.com.tr>
2020-01-28 10:05:57 +0300

    initial commit

---
+++ app/src/main/java/com/example/imavideoview/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.imavideoview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import com.google.ads.interactivemedia.v3.api.*
+import com.google.ads.interactivemedia.v3.api.player.ContentProgressProvider
+import com.google.ads.interactivemedia.v3.api.player.VideoProgressUpdate
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), AdEvent.AdEventListener, AdErrorEvent.AdErrorListener {
+
+    private lateinit var mSdkFactory: ImaSdkFactory
+    private lateinit var mAdsLoader: AdsLoader
+    private lateinit var mAdsManager: AdsManager
+    private var mIsAdDisplay: Boolean = false
+    private lateinit var adsDisplayContainer: AdDisplayContainer
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mSdkFactory = ImaSdkFactory.getInstance()
+        adsDisplayContainer = mSdkFactory.createAdDisplayContainer()
+        adsDisplayContainer.adContainer = videoPlayerWithAdPlayback
+        val settings = mSdkFactory.createImaSdkSettings()
+        mAdsLoader = mSdkFactory.createAdsLoader(this, settings, adsDisplayContainer)
+
+        mAdsLoader.addAdErrorListener(this)
+        mAdsLoader.addAdsLoadedListener {
+            mAdsManager = it.adsManager
+
+            mAdsManager.addAdErrorListener(this)
+            mAdsManager.addAdEventListener(this)
+            mAdsManager.init()
+        }
+
+        videoPlayerView.addVideoCompletedListener(object :
+            VideoPlayerView.OnVideoCompletedListener {
+            override fun onVideoCompleted() = mAdsLoader.contentComplete()
+        })
+
+        playButton.setOnClickListener {
+            videoPlayerView.setVideoPath(getString(R.string.content_url))
+            requestAds(getString(R.string.ad_tag_url))
+            it.visibility = View.GONE
+
+        }
+    }
+
+    private fun requestAds(adTagUrl: String) {
+        val request = mSdkFactory.createAdsRequest()
+        request.adTagUrl = adTagUrl
+        request.contentProgressProvider = object : ContentProgressProvider {
+            override fun getContentProgress(): VideoProgressUpdate {
+
+                if (mIsAdDisplay || videoPlayerView.duration <= 0) {
+                    return VideoProgressUpdate.VIDEO_TIME_NOT_READY
+                }
+                return VideoProgressUpdate(
+                    videoPlayerView.currentPosition.toLong(),
+                    videoPlayerView.duration.toLong()
+                )
+            }
+
+        }
+        mAdsLoader.requestAds(request)
+    }
+
+    override fun onAdEvent(adEvent: AdEvent) {
+
+        when (adEvent.type) {
+            AdEvent.AdEventType.LOADED -> mAdsManager.start()
+            AdEvent.AdEventType.CONTENT_PAUSE_REQUESTED -> {
+                mIsAdDisplay = true
+                videoPlayerView.pause()
+            }
+            AdEvent.AdEventType.CONTENT_RESUME_REQUESTED -> {
+                mIsAdDisplay = true
+                videoPlayerView.play()
+            }
+            AdEvent.AdEventType.ALL_ADS_COMPLETED -> {
+                mAdsManager.destroy()
+            }
+            else -> {
+                //no-op
+            }
+        }
+    }
+
+    override fun onAdError(adError: AdErrorEvent) = videoPlayerView.play()
+
+    override fun onResume() {
+        if (mIsAdDisplay)
+            mAdsManager?.resume()
+        else
+            videoPlayerView.play()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        if (mIsAdDisplay)
+            mAdsManager?.pause()
+        else
+            videoPlayerView.pause()
+        super.onPause()
+    }
+}
---
+++ app/src/main/java/com/example/imavideoview/VideoPlayerView.kt
@@ -0,0 +1,54 @@
+package com.example.imavideoview
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.util.AttributeSet
+import android.widget.MediaController
+import android.widget.VideoView
+import java.lang.UnsupportedOperationException
+
+class VideoPlayerView : VideoView {
+
+    private var onVideoCompletedListener = ArrayList<OnVideoCompletedListener>()
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int)
+            : super(context, attrs, defStyle)
+
+    constructor(context: Context, attrs: AttributeSet)
+            : super(context, attrs)
+
+    constructor(context: Context)
+            : super(context)
+
+    init {
+        val mediaController = MediaController(context)
+        mediaController.setAnchorView(this)
+
+        super.setOnCompletionListener { mp ->
+            mp.reset()
+            mp.setDisplay(holder)
+            for (listener: OnVideoCompletedListener in onVideoCompletedListener)
+                listener.onVideoCompleted()
+
+        }
+
+        super.setOnErrorListener { _, _, _ -> true }
+    }
+
+    override fun setOnCompletionListener(l: MediaPlayer.OnCompletionListener?) =
+        throw UnsupportedOperationException()
+
+
+    public fun play() {
+        start()
+    }
+
+    public fun addVideoCompletedListener(listener: OnVideoCompletedListener) =
+        onVideoCompletedListener.add(listener)
+
+
+    interface OnVideoCompletedListener {
+        fun onVideoCompleted()
+    }
+
+}
