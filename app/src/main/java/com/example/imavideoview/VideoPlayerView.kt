d7a28ac6dd4bf0091b42d3cac9ac943a3c0358c7
Arda TALAY <arda.talay@leanscale.com>
2021-10-10 00:45:27 +0300

    jenkins1

--- app/src/main/java/com/example/imavideoview/VideoPlayerView.kt
+++ app/src/main/java/com/example/imavideoview/VideoPlayerView.kt
@@ -6,7 +6,7 @@ import android.util.AttributeSet
 import android.widget.MediaController
 import android.widget.VideoView
 import java.lang.UnsupportedOperationException
-
+//jenkins1
 class VideoPlayerView : VideoView {
 
     private var onVideoCompletedListener = ArrayList<OnVideoCompletedListener>()
